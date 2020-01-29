dc7ea8b838d7576993689cc60dd64aa2bf16e379
Thanyani Tshikonwani <thanyanitshikonwani@umuzi.org>
2020-01-29 03:53:19 +0200

    second commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,15 +3,13 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args)  {
+    public static void main(String[] args) throws IOException {
         Scanner sinput=new Scanner(System.in);
         String option;
         String quit;
 
-
-       while (true){
-           quit=sinput.nextLine();
-           try {
+//        quit=sinput.nextLine();
+//       while (true){
                System.out.println("Please Press 1 to Add Visitors or Press 2 to view existing visitors" +
                        " Q to exit");
                option=sinput.nextLine();
@@ -20,12 +18,8 @@ public class Main {
                }else if (option.equalsIgnoreCase("2")){
                    FileIO.load();
                }
-           }catch (IOException e){
-               System.err.println("Invalid Inputs");
            }
-           if (quit.equalsIgnoreCase("Q")){
-               return;
+//           if (quit.equalsIgnoreCase("Q"));
            }
-       }
-    }
-}
+//       }
+//    }
